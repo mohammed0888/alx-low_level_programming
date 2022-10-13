64f8896542084d9d94c2b61e07f2823cc3973444
mohammed0888 <m.aldai0888@gmail.com>
2022-10-13 07:43:25 -0700

    gad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-    puts("\"Prgramming is like building a multilingual puzzle");
-    return (0);
+puts("\"Prgramming is like building a multilingual puzzle");
+return (0);
 }
