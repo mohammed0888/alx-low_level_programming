d08880062112ccf2a2d5d7b697b5115ebf7d705e
mohammed0888 <m.aldai0888@gmail.com>
2022-10-18 12:52:48 -0700

    hard woork

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - prints _putchar and a new line 
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
@@ -13,7 +14,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
- _putchar('\n');
-return (0)
-} 
-  
+_putchar('\n');
+return (0);
+}
